239fc035c5dedc9230446f00f0ef78161be999c5
Chang Min <chungmin6363@gmail.com>
2021-12-18 14:30:40 +0800

    add option for data standardization

--- SPECT_MPI_step3_CustomImageDataset.py
+++ SPECT_MPI_step3_CustomImageDataset.py
@@ -2,52 +2,73 @@ from os.path import join
 import pandas as pd
 import nibabel as nib
 import torch
-from torch._C import dtype
+import torch.nn as nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 import glob2 as gb
 
 
 # Creating a Custom Dataset for your files
+# appended channel: = ch1 - ch0
+# output format of images: [C, H, W, D]
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, dataroot):
-        # save all data in the RAM
+    def __init__(self, annotations_file, dataroot, transform=None):
         self.labels = pd.read_csv(annotations_file)
         images_path = gb.glob(join(dataroot, '*.nii'))
-        self.images = [nib.load(path).get_fdata() for path in images_path] 
+        self.images = [nib.load(path).get_fdata() for path in images_path] # save all data in the RAM
+        self.transform = transform
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        label = torch.tensor(self.labels.iloc[idx, 1]) # 2nd column in csv = label (Normal=0, Abnormal=1)
-        image = torch.tensor(self.images[idx], dtype=torch.float32).permute([3,0,1,2]) # permuted tensor would be [Channel x Height x Width x Depth]
-        image_cat = torch.unsqueeze(image[1] - image[0], dim=0) # because inputs sholud have 3 channel
+        # 2nd column in csv contains labels (Normal=0, Abnormal=1)
+        label = torch.tensor(self.labels.iloc[idx, 1])
+        image = torch.tensor(self.images[idx], dtype=torch.float32).permute([3,0,1,2])
+        image_cat = torch.unsqueeze(image[1] - image[0], dim=0) 
         image = torch.cat((image, image_cat), dim=0)
 
+        if self.transform:
+            image = self.transform(image)
+
         return image, label
-     
 
 
-# Test
-if __name__ == '__main__':
+# Computing mean & std for each channel, for Standardization of the dataset.
+# if your RAM can load all samples at onece (like dataset for this project), the code should be much shorter
+# however, this should not always be a case.
+def dataset_statistics(dataset):
+    batch_size = 16
+    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
 
-    # Dataset
-    train_dataset = CustomImageDataset(join('..', 'trainSet.csv'), join('..', 'proc_data_ver0', 'TrainSet'))
-    test_dataset = CustomImageDataset(join('..', 'testSet.csv'), join('..', 'proc_data_ver0', 'TestSet'))
+    mean = torch.zeros(3, dtype=torch.float32)
+    std = torch.zeros(3, dtype=torch.float32)
+    nsample = 0
+    for images, _ in loader:
+        (B, C, H, W, D) = images.shape
+        nsample += B
+        images = images.view(B, C, -1)
+        mean += images.mean(2).sum(0)
+        std += images.std(2).sum(0)
 
-    # WeightedRandomSampler for unbalance data
-    # define sampler using weights which are the reciprocal of the total number of each label category
-    weights = 1. /torch.tensor([sum(train_dataset.labels.iloc[:, 1]==0), sum(train_dataset.labels.iloc[:,1]==1)], dtype=torch.float32)
-    train_target = torch.tensor(train_dataset.labels.iloc[:, 1], dtype=torch.long)
-    sample_weights = weights[train_target]
-    sampler = torch.utils.data.WeightedRandomSampler(sample_weights, num_samples=len(train_dataset), replacement=True)
+    mean /= nsample
+    std /= nsample
 
-    # Data loader (input pipeline)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=16, sampler=sampler)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=16, shuffle=False)
+    return mean, std
 
-    # Show up output tensor.Size
-    for _, (images, labels) in enumerate(train_loader):
-        # print(images.shape)
-        print(labels)
+
+# Display mean & std for each channel, operated across all image processing versions
+def main():
+    
+    nver = 3 # total number of image processing versions
+    for ver in range(nver):
+        foldername = 'proc_data_ver'+ str(ver)
+        train_dataset = CustomImageDataset(join('..', 'trainSet.csv'), join('..', foldername, 'TrainSet'))
+        mean, std = dataset_statistics(train_dataset)
+
+        # These values should be applied for Standardization of the train-dataset before training
+        print(f'Image processing ver {ver} statistics:\n mean = {mean}, std = {std}')
+
+
+if __name__=='__main__':
+    main()
