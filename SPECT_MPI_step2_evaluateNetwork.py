e3703b01d1dc0b8decc08813c2a0e41b305848c8
Chang Min <chungmin6363@gmail.com>
2022-01-13 01:18:40 +0800

    add roc curve

--- SPECT_MPI_step2_evaluateNetwork.py
+++ SPECT_MPI_step2_evaluateNetwork.py
@@ -2,12 +2,37 @@ from os.path import join
 import torch
 import utils.MPIdataset as mpidataset
 from torch.utils.data import DataLoader
+from sklearn.metrics import roc_curve, auc
+import numpy as np
+import matplotlib.pyplot as plt
 from SPECT_MPI_step1_trainNetwork import dataset_statistics
 
 
 # Select image processing version dataset & corresponding trained network
-ver = 'proc_data_ver1'
-model_name = 'Net_epoch5_Batch16_lr0.001.pth'
+ver = 'proc_data_ver0'
+model_name = 'Net_epoch20_Batch16_lr0.001.pth'
+
+def draw_roc_curve(y_true, y_score):
+    fpr, tpr, _ = roc_curve(y_true, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    plt.plot(
+        fpr,
+        tpr,
+        color='darkorange',
+        lw=2,
+        label="ROC curve (area = %0.2f)" % roc_auc,
+    )
+
+    plt.plot([0,1],[0,1],color='navy',lw=2,linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("Receiver operating characteristic")
+    plt.legend(loc="lower right")
+    plt.show()
 
 
 def main(ver, model_name):
@@ -31,33 +56,42 @@ def main(ver, model_name):
     with torch.no_grad():
         total = 0
         correct = 0
-        TP = 0
-        TN = 0
-        FP = 0
-        FN = 0
+        all_scores = []
+        all_predicted = []
+        all_labels = []
 
-        for i, (images, labels) in enumerate(test_loader):
+        for _, (images, labels) in enumerate(test_loader):
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
+            scores, predicted = torch.max(outputs.data, 1)
 
-            # Accuracy
-            total += labels.size(0)
-            correct += (predicted == labels).sum()
+            # Append output
+            all_scores.append(scores)
+            all_predicted.append(predicted)
+            all_labels.append(labels)
+            
+        all_scores = torch.cat(all_scores).cpu()
+        all_predicted = torch.cat(all_predicted).cpu()
+        all_labels = torch.cat(all_labels).cpu()
 
-            # Sensitivity, Specificity
-            TP += torch.logical_and(labels.type(torch.bool)==True, predicted.type(torch.bool)==True).sum()
-            TN += torch.logical_and(labels.type(torch.bool)==False, predicted.type(torch.bool)==False).sum()
-            FP += torch.logical_and(labels.type(torch.bool)==False, predicted.type(torch.bool)==True).sum()
-            FN += torch.logical_and(labels.type(torch.bool)==True, predicted.type(torch.bool)==False).sum()
+        # Accuracy
+        total = all_labels.size(0)
+        correct = (all_predicted == all_labels).sum()
+        
+        # Sensitivity, Specificity
+        TP = torch.logical_and(all_labels.type(torch.bool)==True, all_predicted.type(torch.bool)==True).sum()
+        TN = torch.logical_and(all_labels.type(torch.bool)==False, all_predicted.type(torch.bool)==False).sum()
+        FP = torch.logical_and(all_labels.type(torch.bool)==False, all_predicted.type(torch.bool)==True).sum()
+        FN = torch.logical_and(all_labels.type(torch.bool)==True, all_predicted.type(torch.bool)==False).sum()
                 
-            # Display prediction
-            print(f'The predicted classes in test dataset: {predicted.cpu()}')
-            print(f'The groundtruth classes in test dataset: {labels.cpu()}')
+        # Display evaluations
+        print(f'The predicted classes in test dataset: {all_predicted.cpu()}')
+        print(f'The groundtruth classes in test dataset: {all_labels.cpu()}')
         accuracy = correct.cpu().numpy() / total
         sensitivity = TP / (TP+FN)
         specificity = TN / (FP+TN)
         print(f'Total Samples Number:{total}, Accuracy:{accuracy:.4f}, Sensitivity:{sensitivity:.4f}, Specificity:{specificity:.4f}')
+        draw_roc_curve(all_labels, all_predicted)
 
 
 if __name__ == '__main__':
